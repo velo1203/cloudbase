f5b5aec3356e9cabd246e87725732ac1c9abf2e6
velo1203 <hokitoki1203@gmail.com>
2024-03-02 14:51:53 +0900

    sqlite3 Dependency Installation Failed Error

--- cmd/index.js
+++ cmd/index.js
@@ -6,9 +6,15 @@ const deleteRouter = require("../internal/routes/deleteRouter");
 const logger = require("../internal/middleware/log_request");
 
 const fs = require("fs");
-const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+let config = null;
+try {
+    const data = fs.readFileSync("./config.json");
+    config = JSON.parse(data);
+} catch (err) {
+    console.log("Undefind reading config file");
+}
 const app = express();
-const port = config.port || 3000;
+const port = (config && config.port) || 3000;
 
 app.use(express.json()); // JSON 요청 본문 파싱을 위한 미들웨어
 app.use(logger);
