e95d90e7a9a775b114fef0b788564208f3864bee
velo1203 <hokitoki1203@gmail.com>
2024-03-02 16:51:54 +0900

    라우터 및 미들웨어 변경***
    ***라우터 및 미들웨어를 수정하였습니다. log_request.js 파일을 삭제하고, index.js 파일에서 logger 미들웨어를 제거하였습니다.

--- cmd/index.js
+++ cmd/index.js
@@ -3,14 +3,12 @@ const express = require("express");
 const postRouter = require("../internal/routes/postRouter");
 const readRouter = require("../internal/routes/readRouter");
 const deleteRouter = require("../internal/routes/deleteRouter");
-const logger = require("../internal/middleware/log_request");
 const setPort = require("../internal/bootstrap/port");
 
 const app = express();
 const port = setPort();
 
 app.use(express.json()); // JSON 요청 본문 파싱을 위한 미들웨어
-app.use(logger);
 // 라우터 등록
 app.use(postRouter);
 app.use(readRouter);
